3d1d27ad954ed7a91fca7db5fc42340ecfcb0c40
Aaron <aaronc@swdgroup.com>
2021-11-10 13:28:32 -0500

    sgot api data to render in view

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ const axios = require('axios').default;
 
 const router = express.Router();
 
-const file = "private/key.txt"
-const key = fs.readFileSync(file, 'utf-8');
+const key = fs.readFileSync('private/key.txt', 'utf-8');
+const requestURL = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=${key}&minProtein=50&maxCalories=800&number=10`
+
+// const jsonContent = fs.readFile('private/recipes.json');
+
+let recipes;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get(requestURL)
+  .then((response) => {
+    console.log(response.data);
+    recipes = response.data;
+  });
+  res.render('index', { title: 'food', data: recipes });
 });
-
-const requestURL = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=${key}&minProtein=50&maxCalories=800&number=1`
-
-// axios.get(requestURL).then()
-//   .then((response) => {
-//     console.log(response);
-//   });
-
-const jsonContent = 5; //fs.readFile('private/recipes.json')
 
 module.exports = router;
