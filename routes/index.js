4f6ce0f4525afbccac6d60afdfa2cc16f1e2fdb8
Aaron <aaronc@swdgroup.com>
2021-11-16 18:55:33 -0500

    sharing

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ const router = express.Router();
 const key = fs.readFileSync('private/key.txt', 'utf-8');
 const requestURL = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=${key}&minProtein=50&maxCalories=800&number=10`
 
+let recipes;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   axios.get(requestURL)
   .then((response) => {
-    const recipes = response.data;
-    console.log(recipes)
+    recipes = response.data;
   });
   res.render('index', { title: 'food', data: recipes });
 });
