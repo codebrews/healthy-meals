cf1bc12ad99bc55212aaf0f5bb9c3da9db6c219d
Aaron <aaronc@swdgroup.com>
2021-11-17 09:45:34 -0500

    added detail page

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,24 @@ const axios = require('axios').default;
 const router = express.Router();
 
 const key = fs.readFileSync('private/key.txt', 'utf-8');
-const requestURL = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=${key}&minProtein=50&maxCalories=800&number=10`
-
-let recipes;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.get(requestURL)
+  axios.get(`https://api.spoonacular.com/recipes/findByNutrients?apiKey=${key}&minProtein=50&maxCalories=800&number=10`)
   .then((response) => {
-    recipes = response.data;
+    const recipes = response.data;
+    res.render('index', { title: 'food', data: recipes });
   });
-  res.render('index', { title: 'food', data: recipes });
+});
+
+/* GET detail page. */
+router.get('/:id', function(req, res, next) {
+    console.log(req.params.id);
+  axios.get(`https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=${key}&includeNutrition=true`)
+      .then((response) => {
+        const recipeDetail = response.data;
+        res.render('detail', { title: 'food', data: recipeDetail });
+      });
 });
 
 module.exports = router;
